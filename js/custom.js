e9ef4ec884842f187bacdaed5bafa3a25c1d59f5
yunnmji <yunji3351@naver.com>
2024-07-18 14:47:09 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,245 @@
+// JavaScript Document
+// AOS
+AOS.init({
+    duration: 1200,
+    once: true,
+    disable: 'mobile'
+});
+
+
+$(window).load(function () {
+    "use strict";
+    // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
+
+$(document).ready(function () {
+    "use strict";
+
+    // scroll menu
+    var sections = $('.section'),
+        nav = $('.navbar-fixed-top,footer'),
+        nav_height = nav.outerHeight();
+
+    $(window).on('scroll', function () {
+        var cur_pos = $(this).scrollTop();
+
+        sections.each(function () {
+            var top = $(this).offset().top - nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+
+    nav.find('a').on('click', function () {
+        var $el = $(this),
+            id = $el.attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - nav_height + 2
+        }, 600);
+
+        return false;
+    });
+
+
+    // Menu opacity
+    if ($(window).scrollTop() > 80) {
+        $(".navbar-fixed-top").addClass("bg-nav");
+    } else {
+        $(".navbar-fixed-top").removeClass("bg-nav");
+    }
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 80) {
+            $(".navbar-fixed-top").addClass("bg-nav");
+        } else {
+            $(".navbar-fixed-top").removeClass("bg-nav");
+        }
+    });
+
+    // Parallax
+    var parallax = function () {
+        $(window).stellar();
+    };
+
+    $(function () {
+        parallax();
+    });
+
+    //  isotope
+    $('#projects').waitForImages(function () {
+        var $container = $('.portfolio_container');
+        $container.isotope({
+            filter: '*',
+        });
+
+        $('.portfolio_filter a').click(function () {
+            $('.portfolio_filter .active').removeClass('active');
+            $(this).addClass('active');
+
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 500,
+                    animationEngine: "jquery"
+                }
+            });
+            return false;
+        });
+
+    });
+
+    //skill - easyPieChart
+    $(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.chart').each(function (i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it in */
+            if (bottom_of_window > bottom_of_object) {
+
+                $('.chart').easyPieChart({
+                    barColor: '#1E8842',
+                    trackColor: 'rgba(255,255,255,.5)',
+                    scaleColor: 'transparent',
+                    lineCap: 'round',
+                    lineWidth: 10,
+                    size: 400,
+                    animate: 2000,
+                    onStep: function (from, to, percent) {
+                        $(this.el).find('.percent').text(Math.round(percent));
+                    }
+                });
+            }
+        });
+    });
+
+});
+$(function () {
+    var swiper = new Swiper('.flowslide_inner ', {
+        slidesPerView: 5,//보여지는 갤러리 수
+        spaceBetween: 30,//갤러리 사이 간격
+        speed: 6000,//버튼을 슬라이드가 넘어가는 시간
+        autoplay: {
+            delay: 0,//자동으로 넘어가기 전 머무르는 시간
+            disableOnInteraction: false,
+        },
+        loop: true,//슬라이드 무한반복
+    });
+
+    $('.flowslide .swiper-slide').on('mouseover', function () {
+        swiper.autoplay.stop();
+    });
+    $('.flowslide .swiper-slide').on('mouseout', function () {
+        swiper.autoplay.start();
+    });
+
+
+    // popup
+    $('.art1').click(function () {
+        $('.pop1').fadeIn();
+    });
+    $('.art2').click(function () {
+        $('.pop2').fadeIn();
+    });
+    $('.art3').click(function () {
+        $('.pop3').fadeIn();
+    });
+    $('.art4').click(function () {
+        $('.pop4').fadeIn();
+    });
+    $('.art5').click(function () {
+        $('.pop5').fadeIn();
+    });
+    $('.art6').click(function () {
+        $('.pop6').fadeIn();
+    });
+    $('.art7').click(function () {
+        $('.pop7').fadeIn();
+    });
+
+    $('.popup i').click(function () {
+        $('.popup').fadeOut();
+    });
+});
+
+const cursor = document.querySelector('#cursor');
+const cursorCircle = cursor.querySelector('.cursor__circle');
+
+const mouse = { x: -100, y: -100 }; // mouse pointer's coordinates
+const pos = { x: 0, y: 0 }; // cursor's coordinates
+const speed = 0.1; // between 0 and 1
+
+const updateCoordinates = e => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+}
+
+window.addEventListener('mousemove', updateCoordinates);
+
+function getAngle(diffX, diffY) {
+    return Math.atan2(diffY, diffX) * 180 / Math.PI;
+}
+
+function getSqueeze(diffX, diffY) {
+    const distance = Math.sqrt(
+        Math.pow(diffX, 2) + Math.pow(diffY, 2)
+    );
+    const maxSqueeze = 0.15;
+    const accelerator = 1500;
+    return Math.min(distance / accelerator, maxSqueeze);
+}
+
+const updateCursor = () => {
+    const diffX = Math.round(mouse.x - pos.x);
+    const diffY = Math.round(mouse.y - pos.y);
+
+    pos.x += diffX * speed;
+    pos.y += diffY * speed;
+
+    const angle = getAngle(diffX, diffY);
+    const squeeze = getSqueeze(diffX, diffY);
+
+    const scale = 'scale(' + (1 + squeeze) + ', ' + (1 - squeeze) + ')';
+    const rotate = 'rotate(' + angle + 'deg)';
+    const translate = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+
+    cursor.style.transform = translate;
+    cursorCircle.style.transform = rotate + scale;
+};
+
+function loop() {
+    updateCursor();
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+const cursorModifiers = document.querySelectorAll('[cursor-class]');
+
+cursorModifiers.forEach(curosrModifier => {
+    curosrModifier.addEventListener('mouseenter', function () {
+        const className = this.getAttribute('cursor-class');
+        cursor.classList.add(className);
+    });
+
+    curosrModifier.addEventListener('mouseleave', function () {
+        const className = this.getAttribute('cursor-class');
+        cursor.classList.remove(className);
+    });
+});
